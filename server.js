24ae5ff4af7c8d06cdc3f3fa792ad86e99f5e89a
https://github.com/suza-subedi <suza-subedi>
2021-03-25 19:07:11 +0700

    removed all cors headders

--- server.js
+++ server.js
@@ -24,12 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://cloud-cafe-webapp.herokuapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http:localhost:3000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
